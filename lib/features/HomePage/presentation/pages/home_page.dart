c2470cf0f7b038e8b01be9a65d6a762485bcbd2b
Prince Kumar Yadav <princekumar@Princes-MacBook-Pro.local>
2024-12-12 16:36:59 +0530

    calendar view step done

--- lib/features/HomePage/presentation/pages/home_page.dart
+++ lib/features/HomePage/presentation/pages/home_page.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: CalendarWidget()),
+      body: SafeArea(child: Text("hello")),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:iconcal/app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
