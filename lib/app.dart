4cd3830e327b58bd6fb74b7c5f916c5b6fb5b67e
Prince Kumar Yadav <princekumar@Princes-MacBook-Pro.local>
2024-12-16 20:38:59 +0530

    code updated

--- lib/app.dart
+++ lib/app.dart
@@ -42,7 +42,7 @@ class _MyAppState extends State<MyApp> {
       final eventsResult = await _deviceCalendarPlugin.retrieveEvents(
           calendar?.id,
           RetrieveEventsParams(
-              startDate: DateTime.now(),
+              startDate: DateTime(2024, 1, 1),
               endDate: DateTime.now().add(Duration(days: 365))));
       if (eventsResult.isSuccess) {
         setState(() {
@@ -121,103 +121,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-@override
-Widget build(BuildContext context) {
-  return CalendarControllerProvider(
-    controller: EventController()..addAll(_events),
-    child: MaterialApp(
-      title: 'Flutter Calendar Page Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      scrollBehavior: ScrollBehavior().copyWith(
-        dragDevices: {
-          PointerDeviceKind.trackpad,
-          PointerDeviceKind.mouse,
-          PointerDeviceKind.touch,
-        },
-      ),
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-        ),
-        body: MonthView(
-          cellBuilder: (date, event, isToday, isInMonth, hideDaysNotInMonth) {
-            return Container(
-                margin: EdgeInsets.all(4.0),
-                decoration: BoxDecoration(
-                  color: isToday ? Colors.blue.shade100 : Colors.white,
-                  border: Border.all(
-                      color: isInMonth ? Colors.grey : Colors.transparent),
-                  borderRadius: BorderRadius.circular(8.0),
-                ));
-          },
-        ),
-      ),
-    ),
-  );
-}
-
-List<CalendarEventData> _events = [
-  CalendarEventData(
-    date: _now,
-    title: "Project meeting",
-    description: "Today is project meeting.",
-    startTime: DateTime(_now.year, _now.month, _now.day, 18, 30),
-    endTime: DateTime(_now.year, _now.month, _now.day, 22),
-  ),
-  CalendarEventData(
-    date: _now.add(Duration(days: 1)),
-    startTime: DateTime(_now.year, _now.month, _now.day, 18),
-    endTime: DateTime(_now.year, _now.month, _now.day, 19),
-    title: "Wedding anniversary",
-    description: "Attend uncle's wedding anniversary.",
-  ),
-  CalendarEventData(
-    date: _now,
-    startTime: DateTime(_now.year, _now.month, _now.day, 14),
-    endTime: DateTime(_now.year, _now.month, _now.day, 17),
-    title: "Football Tournament",
-    description: "Go to football tournament.",
-  ),
-  CalendarEventData(
-    date: _now.add(Duration(days: 3)),
-    startTime: DateTime(_now.add(Duration(days: 3)).year,
-        _now.add(Duration(days: 3)).month, _now.add(Duration(days: 3)).day, 10),
-    endTime: DateTime(_now.add(Duration(days: 3)).year,
-        _now.add(Duration(days: 3)).month, _now.add(Duration(days: 3)).day, 14),
-    title: "Sprint Meeting.",
-    description: "Last day of project submission for last year.",
-  ),
-  CalendarEventData(
-    date: _now.subtract(Duration(days: 2)),
-    startTime: DateTime(
-        _now.subtract(Duration(days: 2)).year,
-        _now.subtract(Duration(days: 2)).month,
-        _now.subtract(Duration(days: 2)).day,
-        14),
-    endTime: DateTime(
-        _now.subtract(Duration(days: 2)).year,
-        _now.subtract(Duration(days: 2)).month,
-        _now.subtract(Duration(days: 2)).day,
-        16),
-    title: "Team Meeting",
-    description: "Team Meeting",
-  ),
-  CalendarEventData(
-    date: _now.subtract(Duration(days: 2)),
-    startTime: DateTime(
-        _now.subtract(Duration(days: 2)).year,
-        _now.subtract(Duration(days: 2)).month,
-        _now.subtract(Duration(days: 2)).day,
-        10),
-    endTime: DateTime(
-        _now.subtract(Duration(days: 2)).year,
-        _now.subtract(Duration(days: 2)).month,
-        _now.subtract(Duration(days: 2)).day,
-        12),
-    title: "Chemistry Viva",
-    description: "Today is Joe's birthday.",
-  ),
-];
